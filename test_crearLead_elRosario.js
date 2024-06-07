c4f37d1acd58a68b911fe77e248498a1ab79e237
carlosLizaolah <carlos.lizaola@outlook.com>
2024-06-07 00:54:40 -0600

    Ejecutable El Rosario
    Contiene la data para ejecución de la prueba

---
+++ test_crearLead_elRosario.js
@@ -0,0 +1,13 @@
+//Crear Lead El Rosario test
+
+import {Selector} from 'testcafe';
+import NewLead from './newLeadPage';
+
+
+fixture('El Rosario').page('https://aplatamqa.com/portal-alumno/login?schoolId=36');
+
+test('Crear lead', async t => {
+
+	await NewLead.createLead('prueba240517001@prueba.com', 'Test.123', 'Test.123', 'Prueba', 'Prueba', '6016014567');
+
+});
